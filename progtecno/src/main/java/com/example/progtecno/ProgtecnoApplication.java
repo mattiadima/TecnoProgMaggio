ed796ef5b0749919747bd12065fed404db974fc7
DESKTOP-73HQ3LE\Utente <mattiadima06@gmail.com>
2025-05-04 22:27:24 +0200

    Creazione progetto vuoto con spring

---
+++ progtecno/src/main/java/com/example/progtecno/ProgtecnoApplication.java
@@ -0,0 +1,13 @@
+package com.example.progtecno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgtecnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgtecnoApplication.class, args);
+	}
+
+}
---
+++ progtecno/src/test/java/com/example/progtecno/ProgtecnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progtecno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgtecnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
