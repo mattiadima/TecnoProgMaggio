fa2fdc9a5c995201161c22f5fc9cbf750c2fa7ed
DESKTOP-73HQ3LE\Utente <mattiadima06@gmail.com>
2025-05-10 15:24:46 +0200

    aggiornamento back end

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.progtecno;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProgtecnoApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProgtecnoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.progtecno;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProgtecnoApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
